1ea36a08ed9f15a2f32871d5ba1c092f220a27a9
Jose Valencia <jose.valencia@ctsturismo.cl>
2023-12-03 18:23:52 -0300

    feat: update Navbar

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,38 +1,83 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const NuxtLink = resolveComponent("nuxt-link");
+const navigation = [
+    { name: "Colegios", href: "/colegio" },
+    { name: "Autoridades", href: "/autoridad" },
+];
+</script>
 <template>
-  <header class="sticky top-0 w-full shadow bg-slate-50 text-slate-800 z-50">
-    <BannerUnderConstruction class="" />
-    <div
-      class="max-w-7xl mx-auto flex justify-between items-center px-4 text-sm py-2"
-    >
-      <NuxtLink
-        to="/"
-        class="flex items-center gap-x-2 text-indigo-800 font-bold text-xl uppercase"
-      >
-        <NuxtImg
-          src="https://curacavi.s3.amazonaws.com/logo.webp"
-          class="h-8 w-8"
-        />
-        <p>Curacaví</p>
-      </NuxtLink>
-      <div class="flex gap-x-2">
-        <NuxtLink
-          to="/colegio"
-          active-class="text-indigo-500"
-          class="flex items-center gap-x-2 text-indigo-950 font-semibold hover:text-indigo-600 transition-all duration-500 ease-in-out text-sm uppercase"
-        >
-          Colegios
-        </NuxtLink>
-        <NuxtLink
-          to="/autoridad"
-          active-class="text-indigo-500"
-          activ
-          class="flex items-center gap-x-2 text-indigo-950 font-semibold hover:text-indigo-600 transition-all duration-500 ease-in-out text-sm uppercase"
-        >
-          Autoridades
-        </NuxtLink>
-      </div>
-      <!-- <TheLanguageSelector /> -->
-    </div>
-  </header>
+    <header class="sticky top-0 w-full shadow bg-slate-50 text-slate-800 z-50">
+        <BannerUnderConstruction class="" />
+        <Disclosure as="nav" class="bg-slate-50" v-slot="{ open }">
+            <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                <div class="relative flex h-16 items-center justify-between">
+                    <div
+                        class="absolute inset-y-0 right-0 flex items-center sm:hidden"
+                    >
+                        <!-- Mobile menu button-->
+                        <DisclosureButton
+                            class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-indigo-950 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                        >
+                            <span class="absolute -inset-0.5" />
+                            <span class="sr-only">Open main menu</span>
+                            <Icon
+                                v-if="!open"
+                                name="heroicons:bars-3-20-solid"
+                                class="block h-6 w-6"
+                                aria-hidden="true"
+                            />
+                            <Icon
+                                v-else
+                                name="heroicons:x-mark-20-solid"
+                                class="block h-6 w-6"
+                                aria-hidden="true"
+                            />
+                        </DisclosureButton>
+                    </div>
+                    <div
+                        class="flex flex-1 items-center justify-start sm:items-stretch sm:justify-between"
+                    >
+                        <div class="flex flex-shrink-0 items-center">
+                            <NuxtLink
+                                to="/"
+                                class="flex items-center gap-x-2 text-indigo-800 font-bold text-xl uppercase"
+                            >
+                                <NuxtImg
+                                    src="https://curacavi.s3.amazonaws.com/logo.webp"
+                                    class="h-8 w-8"
+                                />
+                                <p>Curacaví</p>
+                            </NuxtLink>
+                        </div>
+                        <div class="hidden sm:ml-6 sm:block">
+                            <div class="flex space-x-4">
+                                <NuxtLink
+                                    v-for="item in navigation"
+                                    :key="item.name"
+                                    :to="item.href"
+                                    class="text-indigo-900 hover:text-indigo-950 hover:bg-indigo-50 rounded-md px-3 py-2 text-sm font-medium"
+                                    active-class="bg-indigo-50 text-indigo-500"
+                                    >{{ item.name }}</NuxtLink
+                                >
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <DisclosurePanel class="sm:hidden">
+                <div class="space-y-1 px-2 pb-3 pt-2">
+                    <DisclosureButton
+                        v-for="item in navigation"
+                        :key="item.name"
+                        :as="NuxtLink"
+                        :to="item.href"
+                        active-class="bg-indigo-50 text-indigo-950"
+                        class="text-indigo-900 hover:bg-indigo-50 hover:text-indigo-950 block rounded-md px-3 py-2 text-base font-medium"
+                        >{{ item.name }}</DisclosureButton
+                    >
+                </div>
+            </DisclosurePanel>
+        </Disclosure>
+    </header>
 </template>
