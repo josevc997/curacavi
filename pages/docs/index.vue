d39c37d47996c0781df66fac2c0ca9fdced95093
Jose Valencia <jose.valencia@ctsturismo.cl>
2023-12-28 19:23:43 -0300

    feat: try sort again

--- pages/docs/index.vue
+++ pages/docs/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-const docsQuery = await queryContent("docs").sort({ created_at: -1 });
+const docsQuery = queryContent("docs").sort({ created_at: -1 });
 const { data: navigation } = await useAsyncData("navigation", () =>
     fetchContentNavigation(docsQuery)
 );
