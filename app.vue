fc8a1fd9b5e6ada9c498e7a64d956a2325f5df1f
Jose Valencia <jose.valencia@ctsturismo.cl>
2024-01-09 09:06:09 -0300

    feat: add hotjar

--- app.vue
+++ app.vue
@@ -1,19 +1,27 @@
 <script lang="ts" setup>
+import Hotjar from "@hotjar/browser";
+
+const config = useRuntimeConfig();
+
+const siteId = 3818691;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
 useHead({
-  title: "Curacavi",
-  meta: [
-    {
-      hid: "description",
-      name: "description",
-      content: "All information about Curacavi",
-    },
-  ],
+    title: "Curacavi",
+    meta: [
+        {
+            hid: "description",
+            name: "description",
+            content: "All information about Curacavi",
+        },
+    ],
 });
 </script>
 <template>
-  <div>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-  </div>
+    <div>
+        <NuxtLayout>
+            <NuxtPage />
+        </NuxtLayout>
+    </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 const path = require("path");
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    runtimeConfig: {
+        public: {
+            hotjarId: "",
+        },
+    },
     devtools: { enabled: true },
     css: ["~/assets/css/main.css"],
     postcss: {
