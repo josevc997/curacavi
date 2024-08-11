dedd0cde68af5f483979268c2261d94e6982702a
Jose Valencia <jose.valencia@ctsturismo.cl>
2024-08-10 23:21:08 -0400

    feat: add link to customParagraph

--- components/content/MyCustomParagraph.vue
+++ components/content/MyCustomParagraph.vue
@@ -1,6 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { Elemento } from "~/types/blog";
+defineProps<{
+  elemento: Elemento;
+}>();
+</script>
 <template>
-    <p class="py-2">
-        <slot />
-    </p>
+  <p class="py-2">
+    <slot />
+    <template v-for="item in elemento?.children">
+      <NuxtLink
+        v-if="item.tag === 'Enlace'"
+        :to="item.contenido"
+        class="mx-1 font-medium text-blue-600 underline"
+      >
+        {{ item.children[0]?.contenido }}
+      </NuxtLink>
+      <template v-if="item.tag === 'p'">
+        {{ item.contenido }}
+      </template>
+    </template>
+  </p>
 </template>
