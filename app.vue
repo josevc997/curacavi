2e42641a36e5a3c19567766697d18958fb57542a
Jose Valencia <jose.valencia@ctsturismo.cl>
2024-08-17 14:01:02 -0400

    feat: add sentry cdn

--- app.vue
+++ app.vue
@@ -1,19 +1,26 @@
 <script lang="ts" setup>
 useHead({
-    title: "Curacavi",
-    meta: [
-        {
-            hid: "description",
-            name: "description",
-            content: "All information about Curacavi",
-        },
-    ],
+  title: "Curacavi",
+  meta: [
+    {
+      hid: "description",
+      name: "description",
+      content: "All information about Curacavi",
+    },
+  ],
+  script: [
+    {
+      src: "https://js.sentry-cdn.com/194ca471f30166638276235261300c80.min.js",
+      crossorigin: "anonymous",
+      async: true,
+    },
+  ],
 });
 </script>
 <template>
-    <div>
-        <NuxtLayout>
-            <NuxtPage />
-        </NuxtLayout>
-    </div>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
